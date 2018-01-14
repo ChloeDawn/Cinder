7d3c3f77b8ee6aff4984a8c61b2157d6a3ea412a
InsomniaKitten <admin@designdevils.co.uk>
2018-01-14 02:08:21 +0000

    Add debug text.. but the tile entity is going byebye soon.

--- src/main/java/net/insomniakitten/cinder/CinderEvents.java
+++ src/main/java/net/insomniakitten/cinder/CinderEvents.java
@@ -3,19 +3,48 @@ package net.insomniakitten.cinder;
 import net.insomniakitten.cinder.api.IDecayableLight;
 import net.insomniakitten.cinder.api.TileDecayableLight;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.settings.GameSettings;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.client.FMLClientHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber(modid = Cinder.ID)
 public final class CinderEvents {
 
     private CinderEvents() {}
+
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public static void onRenderGameOverlayText(RenderGameOverlayEvent.Text event) {
+        FMLClientHandler handler = FMLClientHandler.instance();
+        GameSettings settings = handler.getClient().gameSettings;
+        if (settings.showDebugInfo && !settings.reducedDebugInfo) {
+            EntityPlayer player = handler.getClientPlayerEntity();
+            if (player != null && player.world != null) {
+                RayTraceResult result = handler.getClient().objectMouseOver;
+                if (result != null && result.typeOfHit == RayTraceResult.Type.BLOCK) {
+                    TileEntity tile = player.world.getTileEntity(result.getBlockPos());
+                    if (tile instanceof TileDecayableLight) {
+                        NBTTagCompound nbt = tile.getUpdateTag();
+                        int light = nbt.getInteger("light");
+                        event.getRight().add("light: " + light);
+                    }
+                }
+            }
+        }
+    }
 
     @SubscribeEvent
     public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
@@ -43,12 +72,12 @@ public final class CinderEvents {
         if (!world.isRainingAt(pos.up()) && light.amount() > 1) {
             light.decay();
             idl.onLightDecay(state, world, pos);
-        } else {
+        } else if (light.amount() > 0) {
             light.extinguish();
             idl.onExtinguished(state, world, pos);
         }
 
-        if (world.rand.nextInt(3) == 0) {
+        if (light.amount() > 0 && world.rand.nextInt(3) == 0) {
             EnumFacing side = EnumFacing.VALUES[world.rand.nextInt(5) + 1];
             if (world.isAirBlock(pos.offset(side))) {
                 IBlockState fire = Blocks.FIRE.getDefaultState();
