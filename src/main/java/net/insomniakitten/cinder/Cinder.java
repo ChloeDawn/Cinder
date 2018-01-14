0e109d8dd4f5cfa0e9395ea219871d4d3b0bdeeb
InsomniaKitten <admin@designdevils.co.uk>
2018-01-14 02:02:14 +0000

    I am very smart and cool, yes.

--- src/main/java/net/insomniakitten/cinder/Cinder.java
+++ src/main/java/net/insomniakitten/cinder/Cinder.java
@@ -1,10 +1,12 @@
 package net.insomniakitten.cinder;
 
+import net.insomniakitten.cinder.api.TileDecayableLight;
 import net.insomniakitten.cinder.block.BlockDecayableTorch;
 import net.minecraft.block.Block;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
 
 @Mod(modid = Cinder.ID, name = Cinder.NAME, version = Cinder.VERSION)
 @Mod.EventBusSubscriber(modid = Cinder.ID)
@@ -16,6 +18,7 @@ public final class Cinder {
 
     @SubscribeEvent
     public static void onBlockRegistry(RegistryEvent.Register<Block> event) {
+        GameRegistry.registerTileEntity(TileDecayableLight.class, ID + ":decayable_light");
         event.getRegistry().register(new BlockDecayableTorch().setRegistryName("minecraft", "torch"));
     }
 
